b42c289abe92dda82bbece906ebb5d8199ae8a00
per6 <rnoah1214@gmail.com>
2019-10-21 13:59:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/questionnaire/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.questionnaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
